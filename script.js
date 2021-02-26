d420ab2cfc37601dc1e4a46267959655bcb7ac9c
ArmeethGarry <armeeth2007@yandex.ru>
2021-02-26 17:42:08 +0300

    using method Object.assign and JSON

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 'use strict'
-//
-// поверхностное клониронание объекта с помощью SPRED оператора
-//
+// -|- КЛОНИРОВАНИЕ ОБЪЕКТА -|-
+
 const userGarry = {
     nikname: 'Garry',
     group: 'Dev',
@@ -9,6 +8,31 @@ const userGarry = {
     admin: false,
 }
 
+//            ПОВЕРХНОСТНОЕ КЛОНИРОВАНИЕ ОБЪЕКТА
+// поверхностное клониронание объекта с помощью SPRED оператора
+
 const userDenis = {...userGarry};
 userDenis.nikname = 'Denis'; // объект userDenis = {nikname: "Denis", group: "Dev", access: 5, admin: false}
                              // объект userGarry без изменения
+
+// поверхностное клониронание объекта с помощью Object.assign
+
+const userNataly = Object.assign( {}, userGarry );
+userNataly.nikname = 'Nataly'; // объект userNataly = {nikname: "Nataly", group: "Test", access: 5, admin: false}
+userNataly.group = 'Test';     // объект userGarry остается без изменения
+
+//       ГЛУБОКОЕ КЛОНИРОВАНИЕ ОБЪЕКТА
+// глубокое клонирование объекта с помощью JSON
+
+const user = {
+    class: '10a',
+    teacher: 'Jhon Lenon',
+    parents: {
+        mom: 'Joan',
+        dad: 'Peter',
+    }
+}
+
+const userAlex = JSON.parse( JSON.stringify( user ) );
+userAlex.parents.mom = 'Victoria'; // объект parents примет значение: {mom: "Victoria", dad: "Peter"}
+                                   // тогда как исходный останется без изменения
